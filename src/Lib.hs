c9cc8a4148cdda5ff682d15a85c2258475697375
Josef Vlach <vlach.josef@gmail.com>
2018-10-03 18:09:57 +0100

    Added DbQuery effect

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,6 +16,7 @@ module Lib where
 
 import           Control.Monad        (join)
 import           Control.Monad.Reader (ReaderT (..))
+import           Data.Char            (chr)
 import           Data.Functor         (Functor)
 
 newtype Id = Id Int deriving Show
@@ -27,8 +28,8 @@ newtype ChangeAccum = ChangeAccum
 
 -- | Auxiliary function for testing of baseMModifyAccum
 -- | Increment Id by one (I bet there is lens one-liner for this)
-incRes :: Result -> [(Id, Value)]
-incRes (Result res) = join (incIds <$> (unChangeAccum <$> res))
+incRes :: [ChangeAccum] -> [(Id, Value)]
+incRes res = join (incIds <$> (unChangeAccum <$> res))
   where
     incIds :: [(Id, Value)] -> [(Id, Value)]
     incIds xs = incId <$> xs
@@ -38,19 +39,32 @@ incRes (Result res) = join (incIds <$> (unChangeAccum <$> res))
 
 -- | Core follows | --
 
+data DbAccess id value res = DbQuery id (value -> res)
+
 data DbAccessM chgAccum id value res
     = DbModifyAccum [(id, value)] ([chgAccum] -> res)
+    | DbAccess (DbAccess id value res)
+
+data DbAccessActions chgAccum id value m = DbAccessActions
+    { daaGetter :: chgAccum -> id -> m value
+    }
 
 data DbAccessActionsM chgAccum id value m = DbAccessActionsM
-    { daaModifyAccum :: chgAccum -> [(id, value)] -> m [chgAccum]
+    { daaAccess      :: DbAccessActions chgAccum id value m
+    , daaModifyAccum :: chgAccum -> [(id, value)] -> m [chgAccum]
     }
 
 class DbActions (effect :: * -> *) (actions :: (* -> *) -> *) param (m :: * -> *) where
     -- | Execute @effect r@ using @actions m@ and return value @r@ in monad @m@.
     executeEffect :: effect r -> actions m -> param -> m r
 
+instance Functor m => DbActions (DbAccess p r)
+                                (DbAccessActions chgAccum p r) chgAccum m where
+    executeEffect (DbQuery p cont) daa chgAccum = cont <$> daaGetter daa chgAccum p
+
 instance Functor m => DbActions (DbAccessM chgAccum id value)
                                 (DbAccessActionsM chgAccum id value) chgAccum m where
+    executeEffect (DbAccess da) (daaAccess -> daa) chgAccum = executeEffect da daa chgAccum
     executeEffect (DbModifyAccum chgSet cont) daaM chgAccum =
         cont <$> daaModifyAccum daaM chgAccum chgSet
 
@@ -129,10 +143,16 @@ runERoCompIO daa initChgAccum comp = runBaseMIO comp ctx
       , _ctxExec     = BaseMIOExec $ \(getCAOrDefault . _ctxChgAccum -> chgAccum) da -> executeEffect da daa chgAccum
       }
 
+testDba :: DbAccessActions ChangeAccum Id Value IO
+testDba = DbAccessActions
+  { daaGetter = \chgAccum (Id id) -> pure (Value $ chr id)
+  }
+
 -- | Represented by sumChangeSetDaaM in blockchain-util in Snowdrop.Execution.DbActions.Simple (Simple.hs)
 testDbaM :: DbAccessActionsM ChangeAccum Id Value IO
 testDbaM = DbAccessActionsM
-    { daaModifyAccum = liftA' modifyAccum
+    { daaAccess = testDba
+    , daaModifyAccum = liftA' modifyAccum
     }
 
     where
@@ -149,13 +169,34 @@ baseMModifyAccum idVals = effect (dbModifyAccum idVals)
 dbModifyAccum :: [(Id, Value)] -> DbAccessM ChangeAccum Id Value Result
 dbModifyAccum idVals = DbModifyAccum idVals (\chgAccum -> (Result chgAccum))
 
+baseMQuery :: BaseM (DbAccess Id Value) ctx Result
+baseMQuery = effect dbQuery
+
+dbQuery :: DbAccess Id Value Result
+dbQuery  = DbQuery (Id 42) (\chgAccum -> (Result [ChangeAccum [(Id 111, chgAccum)]]))
+
 getCAOrDefault :: ChgAccumCtx ctx -> ChgAccum ctx
 getCAOrDefault (CAInitialized cA) = cA
 
 calculation :: [(Id, Value)] -> BaseM (DbAccessM ChangeAccum Id Value) ctx Result
 calculation idVals = do
-    result <- baseMModifyAccum idVals
-    baseMModifyAccum $ incRes result
+    (Result res1) <- convertEffect baseMQuery
+    (Result res2) <- baseMModifyAccum idVals
+    baseMModifyAccum $ ((incRes res2) ++ (join (unChangeAccum <$> res1)))
 
 run :: IO Result
-run = runERoCompIO testDbaM (ChangeAccum $ [(Id 0, Value '*')]) (calculation [(Id 1, Value 'A'), (Id 2, Value 'B'), (Id 3, Value 'C')])
+run = runERoCompIO testDbaM (ChangeAccum $ [(Id 0, Value 'I')]) (calculation [(Id 1, Value 'A'), (Id 2, Value 'B'), (Id 3, Value 'C')])
+
+class ConvertEffect ctx eff1 eff2 where
+    convertEffect :: BaseM eff1 ctx a -> BaseM eff2 ctx a
+
+newtype DbAccessT (eff1 :: * -> * -> * -> *) (eff2 :: * -> * -> * -> *) m a = DbAccessT { runDbAccessT :: m a }
+    deriving ( Functor, Applicative, Monad)
+
+instance (Effectful (DbAccessM chgAccum id value) m)
+    => Effectful (DbAccess id value)
+                 (DbAccessT DbAccess (DbAccessM chgAccum) m) where
+    effect da = DbAccessT $ effect (DbAccess @chgAccum da)
+
+instance ConvertEffect ctx (DbAccess id value) (DbAccessM chgAccum id value) where
+    convertEffect (BaseM action) = BaseM ( runDbAccessT @DbAccess @(DbAccessM chgAccum) action )
